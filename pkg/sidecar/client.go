62d7f9d3c29ae054aaaccbeb9303689a97a64fef
Yuhui <yuhui@omnistrate.com>
2023-10-05 12:52:34 -0700

    multi instances dsicovery

--- pkg/sidecar/client.go
+++ pkg/sidecar/client.go
@@ -16,9 +16,19 @@ func NewClient(ctx context.Context) *Client {
 	return &Client{ctx, &http.Client{Timeout: 60 * time.Second, Transport: http.DefaultTransport}}
 }
 
-func (c *Client) SendAPIRequest() (*http.Response, error) {
+func (c *Client) StartInstance(instanceId string) (*http.Response, error) {
+	resp, err := http.Post("http://127.0.0.1:49750/instanceStatus/start/"+instanceId, "application/json", nil)
+	if err != nil {
+		log.Printf("Failed start instance:"+instanceId+" %v", err)
+	}
+	log.Printf("Response: %v", resp)
 
-	resp, err := http.Get("http://127.0.0.1:49750/instanceStatus/ports/123")
+	return resp, err
+}
+
+func (c *Client) SendAPIRequest(port string) (*http.Response, error) {
+
+	resp, err := http.Get("http://127.0.0.1:49750/instanceStatus/ports/" + port)
 	if err != nil {
 		log.Printf("Failed to get backends endpoints: %v", err)
 	}
